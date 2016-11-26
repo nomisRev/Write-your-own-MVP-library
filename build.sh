2f254454c6da2327627a72402050e263e1dc89ea
Simon Vergauwen <vergauwen.simon@gmail.com>
2016-11-26 21:20:48 +0100

    Assemble en test

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # This will: compile the project, run lint, run tests under JVM, package apk, check the code quality and run tests on the device/emulator.
 "$PROJECT_DIR"/gradlew --no-daemon --info clean
-"$PROJECT_DIR"/gradlew --no-daemon --info build -PdisablePreDex -PwithDexcount -Dscan
-"$PROJECT_DIR"/gradlew --no-daemon --info connectedAndroidTest -PdisablePreDex -PwithDexcount
+"$PROJECT_DIR"/gradlew --no-daemon --info assemble -PdisablePreDex -PwithDexcount -Dscan
+"$PROJECT_DIR"/gradlew --no-daemon --info test connectedAndroidTest -PdisablePreDex -PwithDexcount
